7e1cfee399b5fee655959958fa5bbe7051158d31
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-04-15 21:02:34 +0530

    Add session functionality

--- recordScores.py
+++ recordScores.py
@@ -13,79 +13,99 @@ GPIO.setup(15, GPIO.IN)
 
 # Initialize the LCD display
 lcd = CharLCD('PCF8574', 0x27)
+userId = ''
 
-userIds = [
-    '6431ad39a9b0891674d62703', 
-    '6431b5665d2f022b78b4845a', 
-    '6432a73e4101583ff0d1b61e', 
-    '6432a7824101583ff0d1b622', 
-    '6432a7994101583ff0d1b625', 
-    '6432a7a64101583ff0d1b628'
-]
-
-scores = []
 
 while True:
-    if len(scores) < 10:
-        if len(scores) < 2:
-            angle = 30
-        elif len(scores) < 4:
-            angle = 60
-        elif len(scores) < 6:
-            angle = 90
-        elif len(scores) < 8:
-            angle = 120
-        else:
-            angle = 150
-            
-        if GPIO.input(31) == GPIO.LOW:
-            scores.append([angle, 10, 1])
-            print(scores)  # Print the scores array after adding a score
-            lcd.clear()  # Clear the display
-            lcd.write_string("[{}, {}, {}]".format(angle, 10, 1))  # Write the score data to the display
-            time.sleep(1)  # Wait for 5 seconds before adding another score
-        elif GPIO.input(29) == GPIO.LOW:
-            scores.append([angle, 10, 2])
-            print(scores)  # Print the scores array after adding a score
-            lcd.clear()  # Clear the display
-            lcd.write_string("[{}, {}, {}]".format(angle, 10, 2))
-            time.sleep(1)  # Wait for 1 second before adding another score
-        elif GPIO.input(11) == GPIO.LOW:
-            scores.append([angle, 10, 3])
-            print(scores)  # Print the scores array after adding a score
-            lcd.clear()  # Clear the display
-            lcd.write_string("[{}, {}, {}]".format(angle, 10, 3))
-            time.sleep(1)  # Wait for 1 second before adding another score
-        elif GPIO.input(13) == GPIO.LOW:
-            scores.append([angle, 10, 4])
-            print(scores)  # Print the scores array after adding a score
-            lcd.clear()  # Clear the display
-            lcd.write_string("[{}, {}, {}]".format(angle, 10, 4))
-            time.sleep(1)  # Wait for 1 second before adding another score
-        elif GPIO.input(15) == GPIO.LOW:
-            scores.append([angle, 10, 5])
-            print(scores)  # Print the scores array after adding a score
-            lcd.clear()  # Clear the display
-            lcd.write_string("[{}, {}, {}]".format(angle, 10, 5))
-            time.sleep(1)  # Wait for 1 second before adding another score
+    try:
+        session_data = requests.get(
+            'https://aimassist-server.onrender.com/api/v1/session/getsession?device=1').json()
+        print(session_data)
+        session_started = session_data['data']['session_started']
+        userId = session_data['data']['userid']
+        sessionId = session_data['data']['_id']
+        
+    except:
+        session_started = False
 
-    else:
-        loading = True
-        while loading:
-            try:
-                print("Sending scores to server...")
-                lcd.clear()  # Clear the display
-                lcd.write_string("Sending Scores")
-                userId = random.choice(userIds)
-                response = requests.post('https://aimassist-server.onrender.com/api/v1/round/postRound', json={'scores': scores, 'userId': userId})
-                if response.status_code == 200:
-                    print("Response received! Start shooting again to send another response")
-                    lcd.clear()  # Clear the display
-                    lcd.write_string("Scores Sent! Start Shooting")
-                    scores = []  # Clear the array if the POST request was successful
-                    loading = False
+    if session_started:
+        scores = []
+
+        while True:
+            if len(scores) < 10:
+                if len(scores) < 2:
+                    angle = 30
+                elif len(scores) < 4:
+                    angle = 60
+                elif len(scores) < 6:
+                    angle = 90
+                elif len(scores) < 8:
+                    angle = 120
                 else:
-                    print("Error: ", response.status_code)
-            except:
-                print("Error: Could not connect to server.")
-            time.sleep(1)  # Wait for 1 second before trying again
+                    angle = 150
+
+                if GPIO.input(31) == GPIO.LOW:
+                    scores.append([angle, 10, 1])
+                    print(scores)  # Print the scores array after adding a score
+                    lcd.clear()  # Clear the display
+                    # Write the score data to the display
+                    lcd.write_string("[{}, {}, {}]".format(angle, 10, 1))
+                    # Wait for 5 seconds before adding another score
+                    time.sleep(1)
+                elif GPIO.input(29) == GPIO.LOW:
+                    scores.append([angle, 10, 2])
+                    print(scores)  # Print the scores array after adding a score
+                    lcd.clear()  # Clear the display
+                    lcd.write_string("[{}, {}, {}]".format(angle, 10, 2))
+                    # Wait for 1 second before adding another score
+                    time.sleep(1)
+                elif GPIO.input(11) == GPIO.LOW:
+                    scores.append([angle, 10, 3])
+                    print(scores)  # Print the scores array after adding a score
+                    lcd.clear()  # Clear the display
+                    lcd.write_string("[{}, {}, {}]".format(angle, 10, 3))
+                    # Wait for 1 second before adding another score
+                    time.sleep(1)
+                elif GPIO.input(13) == GPIO.LOW:
+                    scores.append([angle, 10, 4])
+                    print(scores)  # Print the scores array after adding a score
+                    lcd.clear()  # Clear the display
+                    lcd.write_string("[{}, {}, {}]".format(angle, 10, 4))
+                    # Wait for 1 second before adding another score
+                    time.sleep(1)
+                elif GPIO.input(15) == GPIO.LOW:
+                    scores.append([angle, 10, 5])
+                    print(scores)  # Print the scores array after adding a score
+                    lcd.clear()  # Clear the display
+                    lcd.write_string("[{}, {}, {}]".format(angle, 10, 5))
+                    # Wait for 1 second before adding another score
+                    time.sleep(1)
+
+            else:
+                loading = True
+                while loading:
+                    try:
+                        print("Sending scores to server...")
+                        lcd.clear()  # Clear the display
+                        lcd.write_string("Sending Scores")
+                        response = requests.post(
+                            'https://aimassist-server.onrender.com/api/v1/round/postRound', json={'scores': scores, 'userId': userId})
+
+                        if response.status_code == 200:
+                            print(sessionId)
+                            sessionResponse = requests.post(
+                                'https://aimassist-server.onrender.com/api/v1/session/endsession', json={'sessionId' : sessionId})
+                            if sessionResponse.status_code == 200:                                
+                                print("Response received! Start session again to send another response")
+                                lcd.clear()  # Clear the display
+                                lcd.write_string("Scores Sent! Start session")
+                    
+                                scores = []  # Clear the array if the POST request was successful
+                                loading = False
+                            else: 
+                                print("Error: ", sessionResponse.status_code)
+                        else:
+                            print("Error: ", response.status_code)
+                    except:
+                        print("Error: Could not connect to server.")
+                    time.sleep(1)  # Wait for 1 second before trying again
